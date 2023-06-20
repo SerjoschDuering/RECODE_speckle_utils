c92e3bea0e85ffe669dc3ba360f9a352480125cb
SerjoschDuering <tralala798@gmail.com>
2023-06-20 09:24:02 +0200

    seperate colormaps file

---
+++ color_maps.py
@@ -0,0 +1,10 @@
+def gh_color_blueRed():
+    # grasshoper color scheme 
+    color_list = [[15,16,115],
+            [177,198,242],
+            [251,244,121],
+            [222,140,61],
+            [183,60,34]]
+    # Scale RGB values to [0,1] range
+    color_list = [[c/255. for c in color] for color in color_list]
+    return color_list
